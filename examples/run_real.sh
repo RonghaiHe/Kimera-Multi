239f5aff24c748bba0ad8b8e0ef74b976faca98e
wp2 <wp2@sysu.com>
2025-03-30 11:47:54 +0800

    Feat: Add configuration file to real-world experimnents

--- examples/run_real.sh
+++ examples/run_real.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-CATKIN_WS="/home/robot/catkin_ws" LOG_DIR="/home/" tmuxp load example_real.yaml
+CATKIN_WS_="/home/robot/kimera_multi_ws" 
+DATA_PATH_="/home/robot/dataset/self" 
+
+if [ $# -eq 0 -o "$1" = "c" ]; then
+    echo "Dataset collection"
+    CATKIN_WS=$CATKIN_WS_ \
+    DATA_PATH=$DATA_PATH_ \
+    tmuxp load $CATKIN_WS_/src/kimera_multi/examples/data_collection.yaml
+    
+elif [ "$1" = "r" ]; then
+    echo "Run CSLAM in real-world"    
+    CATKIN_WS=$CATKIN_WS_ \
+    LOG_DIR=${DATA_PATH_}/log_data \
+    tmuxp load $CATKIN_WS_/src/kimera_multi/examples/example_real.yaml
+fi
